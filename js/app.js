cddb373869b3b6ee4658fc174b265f72fada969c
Aseel Hamamreh <aseel.hamamreh@hotmail.com>
2021-03-22 18:10:42 +0200

    Done by Aseel

--- js/app.js
+++ js/app.js
@@ -1,27 +1,305 @@
-let WorkingHours = ['07:00 AM','08:00 AM','09:00 AM','10:00 AM','11:00 AM','12:00 PM','01:00 PM','02:00 PM','03:00 PM','04:00 PM','05:00 PM','06:00 PM','07:00 PM','08:00 PM'];
+let WorkingHours = ['7 AM','8 AM','9 AM','10 AM','11 AM','12 PM','1 PM','2 PM','3 PM','4 PM','5 PM','6 PM','7 PM','8 PM'];
 
+// first Object:
 const Seattle = {
-  Name:'seattle',
+  Location:'seattle',
   MinCust: 23,
   MaxCust: 65,
   AvgCookie: 6.3,
-  day:0,
-  finalAvg:0,
-  AvgCal: function(min,max){
-    min=this.MinCust;
-    max=this.MaxCust;
-    this.day= Math.floor(Math.random() * (max - min + 1) + min);
-    return this.day;
+  AvgCust:0,
+  total:0,
+  CookiesPerHour:[],
+  Results:[],
+  findAvgCust: function(){
+    let min=this.MinCust;
+    let max=this.MaxCust;
+    this.AvgCust = Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.ceil(this.AvgCust);
   },
-  WithArray: function(){
+  findCookiesPerHour: function(){
+    this.total =0;
     for (let i=0; i< WorkingHours.length;i++){
-      this.finalAvg=this.day*this.AvgCookie;
-      return this.finalAvg;
+      this.findAvgCust();
+      this.CookiesPerHour[i]=Math.ceil(this.AvgCust*this.AvgCookie);
+      this.total= this.total+this.CookiesPerHour[i];
+      this.Results=this.CookiesPerHour;
     }
+  },
+
+  // // displaying elements on the site for the first Object:
+  displaying: function(){
+    const container = document.getElementById('First');
+    // console.log(container);
+    const mainEl = document.createElement('main');
+    container.appendChild(mainEl);
+    // console.log(mainEl);
+    const H1El = document.createElement ('h1');
+    mainEl.appendChild(H1El);
+    H1El.textContent= 'Sales Data';
+
+    const UlEl = document.createElement ('ul');
+    mainEl.appendChild(UlEl);
+    UlEl.textContent=`${this.Location} sales:`;
+
+    for (let i=0; i<WorkingHours.length;i++){
+      const LiEl = document.createElement ('li');
+      UlEl.appendChild(LiEl);
+      LiEl.textContent= `${WorkingHours[i]}: ${this.Results[i]} cookies `;
+    }
+    const Li2El = document.createElement ('li');
+    UlEl.appendChild(Li2El);
+    Li2El.textContent= `Total = ${this.total}`;
   }
-
 };
+// cooling first object:
+
+Seattle.findAvgCust();
+console.log(Seattle.AvgCust);
+
+Seattle.findCookiesPerHour();
+console.log(Seattle.Results);
+console.log(Seattle.total);
+
+Seattle.displaying();
+
+// Second Object:
+const Tokyo = {
+  Location:'Tokyo',
+  MinCust: 3,
+  MaxCust: 24,
+  AvgCookie: 1.2,
+  AvgCust:0,
+  total:0,
+  CookiesPerHour:[],
+  Results:[],
+  findAvgCust: function(){
+    let min=this.MinCust;
+    let max=this.MaxCust;
+    this.AvgCust = Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.ceil(this.AvgCust);
+  },
+  findCookiesPerHour: function(){
+    this.total =0;
+    for (let i=0; i< WorkingHours.length;i++){
+      this.findAvgCust();
+      this.CookiesPerHour[i]=Math.ceil(this.AvgCust*this.AvgCookie);
+      this.total= this.total+this.CookiesPerHour[i];
+      this.Results=this.CookiesPerHour;
+    }
+  },
+
+  // // displaying elements on the site for the Tokyo Object:
+  displaying: function(){
+    const container = document.getElementById('First');
+    // console.log(container);
+    const mainEl = document.createElement('main');
+    container.appendChild(mainEl);
+    // console.log(mainEl);
+   
+
+    const UlEl = document.createElement ('ul');
+    mainEl.appendChild(UlEl);
+    UlEl.textContent=`${this.Location} sales:`;
+
+    for (let i=0; i<WorkingHours.length;i++){
+      const LiEl = document.createElement ('li');
+      UlEl.appendChild(LiEl);
+      LiEl.textContent= `${WorkingHours[i]}: ${this.Results[i]} cookies `;
+    }
+    const Li2El = document.createElement ('li');
+    UlEl.appendChild(Li2El);
+    Li2El.textContent= `Total = ${this.total}`;
+  }
+};
+// cooling Tokyo object:
+
+Tokyo.findAvgCust();
+console.log(Tokyo.AvgCust);
+
+Tokyo.findCookiesPerHour();
+console.log(Tokyo.Results);
+console.log(Tokyo.total);
+
+Tokyo.displaying();
 
 
+// Dubai Object:
+const Dubai = {
+  Location:'Dubai',
+  MinCust: 11,
+  MaxCust: 38,
+  AvgCookie: 3.7,
+  AvgCust:0,
+  total:0,
+  CookiesPerHour:[],
+  Results:[],
+  findAvgCust: function(){
+    let min=this.MinCust;
+    let max=this.MaxCust;
+    this.AvgCust = Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.ceil(this.AvgCust);
+  },
+  findCookiesPerHour: function(){
+    this.total =0;
+    for (let i=0; i< WorkingHours.length;i++){
+      this.findAvgCust();
+      this.CookiesPerHour[i]=Math.ceil(this.AvgCust*this.AvgCookie);
+      this.total= this.total+this.CookiesPerHour[i];
+      this.Results=this.CookiesPerHour;
+    }
+  },
+
+  // // displaying elements on the site for the Dubai Object:
+  displaying: function(){
+    const container = document.getElementById('First');
+    // console.log(container);
+    const mainEl = document.createElement('main');
+    container.appendChild(mainEl);
+    // console.log(mainEl);
+   
+
+    const UlEl = document.createElement ('ul');
+    mainEl.appendChild(UlEl);
+    UlEl.textContent=`${this.Location} sales:`;
+
+    for (let i=0; i<WorkingHours.length;i++){
+      const LiEl = document.createElement ('li');
+      UlEl.appendChild(LiEl);
+      LiEl.textContent= `${WorkingHours[i]}: ${this.Results[i]} cookies `;
+    }
+    const Li2El = document.createElement ('li');
+    UlEl.appendChild(Li2El);
+    Li2El.textContent= `Total = ${this.total}`;
+  }
+};
+// cooling Dubai object:
+
+Dubai.findAvgCust();
+console.log(Dubai.AvgCust);
+
+Dubai.findCookiesPerHour();
+console.log(Dubai.Results);
+console.log(Dubai.total);
+
+Dubai.displaying();
+
+// Paris Object:
+const Paris = {
+  Location:'Paris',
+  MinCust: 20,
+  MaxCust: 38,
+  AvgCookie: 2.3,
+  AvgCust:0,
+  total:0,
+  CookiesPerHour:[],
+  Results:[],
+  findAvgCust: function(){
+    let min=this.MinCust;
+    let max=this.MaxCust;
+    this.AvgCust = Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.ceil(this.AvgCust);
+  },
+  findCookiesPerHour: function(){
+    this.total =0;
+    for (let i=0; i< WorkingHours.length;i++){
+      this.findAvgCust();
+      this.CookiesPerHour[i]=Math.ceil(this.AvgCust*this.AvgCookie);
+      this.total= this.total+this.CookiesPerHour[i];
+      this.Results=this.CookiesPerHour;
+    }
+  },
+
+  // // displaying elements on the site for the Paris Object:
+  displaying: function(){
+    const container = document.getElementById('First');
+    // console.log(container);
+    const mainEl = document.createElement('main');
+    container.appendChild(mainEl);
+    // console.log(mainEl);
+
+    const UlEl = document.createElement ('ul');
+    mainEl.appendChild(UlEl);
+    UlEl.textContent=`${this.Location} sales:`;
+
+    for (let i=0; i<WorkingHours.length;i++){
+      const LiEl = document.createElement ('li');
+      UlEl.appendChild(LiEl);
+      LiEl.textContent= `${WorkingHours[i]}: ${this.Results[i]} cookies `;
+    }
+    const Li2El = document.createElement ('li');
+    UlEl.appendChild(Li2El);
+    Li2El.textContent= `Total = ${this.total}`;
+  }
+};
+// cooling Paris object:
+
+Paris.findAvgCust();
+console.log(Paris.AvgCust);
+
+Paris.findCookiesPerHour();
+console.log(Paris.Results);
+console.log(Paris.total);
+
+Paris.displaying();
+
+
+// Lima Object:
+const Lima = {
+  Location:'Lima',
+  MinCust: 2,
+  MaxCust: 16,
+  AvgCookie: 4.6,
+  AvgCust:0,
+  total:0,
+  CookiesPerHour:[],
+  Results:[],
+  findAvgCust: function(){
+    let min=this.MinCust;
+    let max=this.MaxCust;
+    this.AvgCust = Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.ceil(this.AvgCust);
+  },
+  findCookiesPerHour: function(){
+    this.total =0;
+    for (let i=0; i< WorkingHours.length;i++){
+      this.findAvgCust();
+      this.CookiesPerHour[i]=Math.ceil(this.AvgCust*this.AvgCookie);
+      this.total= this.total+this.CookiesPerHour[i];
+      this.Results=this.CookiesPerHour;
+    }
+  },
+
+  // // displaying elements on the site for the Paris Object:
+  displaying: function(){
+    const container = document.getElementById('First');
+    // console.log(container);
+    const mainEl = document.createElement('main');
+    container.appendChild(mainEl);
+    // console.log(mainEl);
+
+    const UlEl = document.createElement ('ul');
+    mainEl.appendChild(UlEl);
+    UlEl.textContent=`${this.Location} sales:`;
+
+    for (let i=0; i<WorkingHours.length;i++){
+      const LiEl = document.createElement ('li');
+      UlEl.appendChild(LiEl);
+      LiEl.textContent= `${WorkingHours[i]}: ${this.Results[i]} cookies `;
+    }
+    const Li2El = document.createElement ('li');
+    UlEl.appendChild(Li2El);
+    Li2El.textContent= `Total = ${this.total}`;
+  }
+};
+// cooling Paris object:
+
+Lima.findAvgCust();
+console.log(Lima.AvgCust);
+
+Lima.findCookiesPerHour();
+console.log(Lima.Results);
+console.log(Lima.total);
+
+Lima.displaying();
 
 
